e3984f102bd36dce059fe2eebfa0e1da0a4e34ea
Marco Carvalho <marcoacarvalho@gmail.com>
2020-04-20 14:57:38 +0100

    Fix unbound variable error

--- push.sh
+++ push.sh
@@ -136,12 +136,12 @@ case "$2" in
 
         NEXUS_RESPONSE=$(curl -is -u "$AUTH" "$REPO_URL" --upload-file "$CHART_PACKAGE" | indent)
         # Generate error code if 400-505
-        if $(echo $nexus_response:- | grep -q 'HTTP/1.1 400'); then
+        if $(echo $NEXUS_RESPONSE:- | grep -q 'HTTP/1.1 400'); then
           echo "${CHART_PACKAGE} already exists in ${REPO_URL}"
-          echo $nexus_response | grep 'HTTP/1.1 400'
+          echo $NEXUS_RESPONSE | grep 'HTTP/1.1 400'
           exit 2
-        elif $(echo $nexus_response:- | grep 'HTTP/1.1' | egrep -q [401-505]); then
-          echo $nexus_response | egrep 'HTTP/1.1 [401-505]'
+        elif $(echo $NEXUS_RESPONSE:- | grep 'HTTP/1.1' | egrep -q [401-505]); then
+          echo $NEXUS_RESPONSE | egrep 'HTTP/1.1 [401-505]'
           exit 1
         else
           echo "${CHART_PACKAGE} uploaded successfully"
